f62c58d3f2b617108d277f222e4c49183386da7f
Henrique Gouveia <henrique.gouveia17@gmail.com>
2019-07-14 22:04:46 -0300

    added auth0 config

---
+++ src/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+    domain: "[YOUR_DOMAIN]",
+    roleUrl: "https://rbac-tutorial-app/role",
+    clientId: "[YOUR_CLIENT_ID]",
+    callbackUrl: "http://localhost:3000/callback"    
+  };
